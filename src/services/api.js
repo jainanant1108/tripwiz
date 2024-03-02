61b81e0645c97b443d19ce6e47d257d4468a1a27
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-03-01 22:43:35 -0500

    Fixes the server not online error

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const tripApi = axios.create({
-  //baseURL: "https://trip-wiz-service.onrender.com",
-  baseURL: "http://localhost:10000",
+  baseURL: "https://trip-wiz-service.onrender.com",
+  //baseURL: "http://localhost:10000",
 });
 
 tripApi.interceptors.response.use(
